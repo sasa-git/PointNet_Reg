7acce71dc8f6f377091cfb16cefc010f8304e925
sasasoni <sasasoni1119@gmail.com>
2020-12-01 12:34:00 +0900

    add pcd visualize

--- show_pcd.py
+++ show_pcd.py
@@ -1,6 +1,12 @@
+# Usage: -> % python3 show_one_face.py TestData/five_position_classes/0/train/0.pcd 
+
 import open3d as o3d
 import numpy as np
+import sys
 
-pcd = o3d.io.read_point_cloud("Data/five_faces_class/0/0.pcd")
+path = sys.argv[1]
+pcd = o3d.io.read_point_cloud(path)
 points = np.array(pcd.points)
+
 print(points.shape)
+o3d.visualization.draw_geometries([pcd])
